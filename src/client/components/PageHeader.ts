a5c04f19b49c91ec9329b0ac342d3a955fe91071
Samuel Allen <dehuszar@gmail.com>
2021-10-06 20:29:45 -0400

    removing tailwindcss in favor of lit styling patterns; adding discrete normalize; adding typefaces, anchor, header patterns; fixing nav active state; moving primary nav into header component

--- src/client/components/PageHeader.ts
+++ src/client/components/PageHeader.ts
@@ -1,22 +1,43 @@
-import { LitElement, html } from 'lit';
+import { LitElement, html, css } from 'lit';
 import { customElement, property } from 'lit/decorators.js';
+import { anchorStyles } from '../css/anchors.css.ts';
+import { headerStyles } from '../css/headers.css.ts';
 import './NavList';
 
 @customElement('page-header')
 export class PageHeader extends LitElement {
-  @property({type: String}) pageTitle = '';
   @property({type: String}) siteTitle = '';
+
+  static get styles() {
+    return [
+      anchorStyles,
+      headerStyles,
+      css`
+        header {
+          display: flex;
+        }
+
+        h1 {
+          padding: 0.5em 1em;
+          margin: 0;
+        }
+
+        a {
+          text-decoration: none;
+        }
+
+        a, a:visited {
+          color: var(--text-color);
+        }
+      `
+    ];
+  }
   render() {
 
     return html`
-      <style>
-        :host {
-          font-size: 1em;
-        }
-      </style>
       <header>
-        <h1 class="site-title">${this.siteTitle}</h1>
-        <h2 class="page-title">${this.pageTitle}</h2>
+        <h1 class="site-title"><a href="/">${this.siteTitle}</a></h1>
+        <slot></slot>
       </header>
     `;
   }
--- src/client/layouts/cv.ts
+++ src/client/layouts/cv.ts
@@ -1,20 +1,28 @@
-import {LitElement, html} from 'lit';
+import {LitElement, html, css} from 'lit';
 import {customElement, property} from 'lit/decorators.js';
 import '../components/PageHeader';
 import '../components/PageContent';
 import '../components/PrimaryNav';
 
 export default data => html`
+  <style>
+    /* FIXME :: this isn't the right place for this */
+    article h1 {
+      /* FIXME :: VAR NOT WORKING WITHOUT SHADOW DOM
+      font-family: var(--sans-serif-font); */
+      font-family: 'Lato', sans-serif;
+      font-size: 1em;
+    }
+  </style>
   <page-header
-    pageTitle="${data.pageTitle}"
     siteTitle="${data.siteTitle}">
+    <primary-nav
+      pageContext=${data.pageContext}></primary-nav>
   </page-header>
-  <primary-nav
-    pageContext=${data.pageContext}></primary-nav>
-  <page-content>
-    <section>
+  <page-content pageTitle="${data.pageTitle}">
+    <section class="education">
       <header>
-        <h2>Education</h2>
+        <h3>Education</h3>
       </header>
       ${JSON.parse(data.content).education.map(p => html`
       <article>
@@ -22,9 +30,9 @@ export default data => html`
       </article>
     `)}
     </section>
-    <section>
+    <section class="experience">
       <header>
-        <h2>Experience</h2>
+        <h3>Experience</h3>
       </header>
       ${JSON.parse(data.content).experience.map(p => html`
       <article>
@@ -32,9 +40,9 @@ export default data => html`
       </article>
       `)}
     </section>
-    <section>
+    <section class="skills">
       <header>
-        <h2>Skills</h2>
+        <h3>Skills</h3>
       </header>
       ${JSON.parse(data.content).skills.map(p => html`
       <article>
