c83c929fcd993cb0676374b125fd74de8620ff69
Samuel Allen <dehuszar@gmail.com>
2023-08-10 13:12:17 -0400

    scrubbing unneeded education history, tightening grid

--- content/cv/education/university-of-washington.md
+++ content/cv/education/university-of-washington.md
@@ -2,7 +2,6 @@
 endYear: "2002"
 location: "Seattle, WA"
 slug: "university-of-washington"
-startYear: "1998"
 tags: []
 title: "University of Washington"
 type: "education"
--- src/components/PageContent.ts
+++ src/components/PageContent.ts
@@ -16,16 +16,15 @@ export class PageContent extends LitElement {
       css`
         :host {
           display: grid;
+          gap: 1em 3em;
           grid-template-columns: repeat(12, 1fr);
           grid-template-rows: repeat(12, 3em);
-          gap: 3em;
           padding: 0 2em;
         }
 
         header {
           grid-row-end: span 1;
-          grid-column-start: 1;
-          grid-column-end: span 12;
+          grid-column: 1 / 12;
         }
 
         header h2 {
@@ -37,33 +36,42 @@ export class PageContent extends LitElement {
           text-decoration: underline;
         }
 
-        ::slotted(*) {
-          grid-row-end: span 6;
-        }
-
         ::slotted(section) {
-          grid-column-end: span 2;
-          display: inline-grid;
+          gap: 1em 3em;
+          grid-column-end: span 4;
+          display: grid;
         }
 
         ::slotted(article) {
-          grid-row-end: span 10;
           grid-column-end: span 8;
           grid-column-start: 3;
+          grid-row-end: span 10;
+        }
+
+        ::slotted(.education) {
+          grid-column 1 / 5;
+          grid-row: 2 / 2;
+          grid-template-columns: repeat(2, 1fr);
         }
 
         ::slotted(.experience) {
-          grid-column-start: 3;
+          grid-column: 1 / 5;
+          grid-row-start: 6;
+          grid-template-columns: repeat(2, 1fr);
         }
 
         ::slotted(.skills) {
-          grid-column-start: 5;
           grid-column-end: span 4;
+          grid-column-start: 5;
+          grid-template-columns: repeat(3, 1fr);
+          grid-template-rows: repeat(9, 3em);
         }
 
         ::slotted(.tools) {
-          grid-column-start: 9;
           grid-column-end: span 4;
+          grid-column-start: 9;
+          grid-template-columns: repeat(3, 1fr);
+          grid-template-rows: repeat(9, 3em);
         }
       `
     ];
