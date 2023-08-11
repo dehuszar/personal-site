cf2c0c46f6a3d8ed36d81627a3e97804704c4ff7
Samuel Allen <dehuszar@gmail.com>
2023-08-10 21:06:56 -0400

    pulling cv experience section out into discrete component

--- src/css/headers.css.ts
+++ src/css/headers.css.ts
@@ -4,8 +4,8 @@ export const headerStyles = css`
   h1, h2, h3, h4, h5, h6 {
     font-family: 'Lora', serif;
   }
-
-  h3 {
-    text-decoration: underline;
+  
+  header h3 {
+    border-bottom: 1px solid black;
   }
 `
---
+++ src/lib/getRecordsByType.ts
@@ -0,0 +1,4 @@
+export const education = r => r.type === "education";
+export const experience = r => r.type === "experience";
+export const skills = r => r.type === "skill";
+export const tools = r => r.type === "tool";
