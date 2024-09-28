f10c0a86e9005b8bcaf003f86bea8c72017857e1
Samuel Allen <dehuszar@gmail.com>
2024-09-28 19:29:19 -0400

    cleaning up old cruft

--- src/lib/getRecordsByType.ts
+++
@@ -1,4 +0,0 @@
-export const education = r => r.type === "education";
-export const experience = r => r.type === "experience";
-export const skills = r => r.type === "skill";
-export const tools = r => r.type === "tool";
