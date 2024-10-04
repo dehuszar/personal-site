09ba0eb78ecd5f25b0511018bd925bb2c3ead530
Samuel Allen <dehuszar@gmail.com>
2024-10-04 18:51:34 -0400

    scrubbing remaining backend js

--- server.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-const app = express();
-const port = 3000
-
-app.use(express.static('build',{extensions:['html']}));
-app.listen(port, () => {
-	console.log("something is happening")
-});
-
